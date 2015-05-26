61e1477115c8ebae2c299e7bdee057d0acb0dd08
pianonerd7 <jxh604@case.edu>
2015-05-26 19:16:17 -0400

    Initial commit

---
+++ BreakReminder/Model/ReminderModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakReminder.Model
+{
+    public class ReminderModel
+    {
+
+        private DateTime _startTime;
+        private long _recurrenceMin;
+        public ReminderModel(DateTime startTime, long recurrenceMin)
+        {
+            _startTime = startTime;
+            _recurrenceMin = recurrenceMin;
+        }
+    }
+}
---
+++ BreakReminder/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakReminder.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged(string propertyName)
+        {
+
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
